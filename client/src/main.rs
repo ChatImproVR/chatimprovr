21b8b2549e073eede62193561385e1d91d177a83
Masterchef365 <duncan.freeman1@gmail.com>
2022-12-14 16:53:54 -0800

    Stress test

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, time::Instant};
 
 use anyhow::Result;
 use cimvr_engine::{
@@ -8,7 +8,7 @@ use cimvr_engine::{
 };
 
 fn main() -> Result<()> {
-    let path: PathBuf = "target/wasm32-unknown-unknown/debug/plugin.wasm".into();
+    let path: PathBuf = "target/wasm32-unknown-unknown/release/plugin.wasm".into();
 
     let wasm = Engine::new(&Default::default())?;
     let mut plugin = Plugin::new(&wasm, &path)?;
@@ -22,6 +22,23 @@ fn main() -> Result<()> {
     })?;
 
     dbg!(recv);
+
+    let t = Instant::now();
+    let n = 500_000;
+    for _ in 0..n {
+        let recv = plugin.dispatch(&ReceiveBuf {
+            system: Some(0),
+            ecs: EcsData {
+                entities: vec![],
+                components: vec![],
+            },
+            messages: vec![],
+        })?;
+    }
+
+    let t = t.elapsed();
+    println!("{}s", t.as_secs_f32());
+    println!("{} FPS", n as f32 / t.as_secs_f32());
 
     Ok(())
 }
