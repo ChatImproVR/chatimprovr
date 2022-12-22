60ecb794fafba94d687633c90c67e5d969fd9fb4
Masterchef365 <duncan.freeman1@gmail.com>
2022-12-21 17:05:28 -0800

    Test server init

---
+++ run_server.sh
@@ -0,0 +1,2 @@
+base="./target/wasm32-unknown-unknown/release"
+cargo run --release --bin cimvr_server -- $base/plugin.wasm $base/plugin2.wasm
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use cimvr_engine::{interface::system::Stage, Engine};
+use std::path::PathBuf;
+
+fn main() -> Result<()> {
+    // Set up logging
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+
+    // Parse args
+    let args = std::env::args().skip(1);
+    let paths: Vec<PathBuf> = args.map(PathBuf::from).collect();
+
+    // Set up engine and initialize plugins
+    let mut engine = Engine::new(&paths, true)?;
+
+    engine.init_plugins()?;
+    engine.dispatch(Stage::Input)?;
+
+    Ok(())
 }
