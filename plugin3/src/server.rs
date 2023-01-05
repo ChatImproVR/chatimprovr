c6951f27e0847ddb74d9e9e918afaa59bbfe416d
Masterchef365 <duncan.freeman1@gmail.com>
2023-01-05 01:42:16 -0800

    Script to compile under docker

--- plugin3/src/server.rs
+++ plugin3/src/server.rs
@@ -8,14 +8,11 @@ use cimvr_engine_interface::{dbg, make_app_state, prelude::*, println};
 use serde::{Deserialize, Serialize};
 
 pub struct ServerState {
-    rgb: [f32; 3],
     cube: EntityId,
 }
 
 impl UserState for ServerState {
     fn new(io: &mut EngineIo, sched: &mut EngineSchedule<Self>) -> Self {
-        let rgb = [1.; 3];
-
         let cube = io.create_entity();
         io.add_component(cube, &Transform::default());
 
@@ -27,22 +24,19 @@ impl UserState for ServerState {
                 .subscribe::<ChangeColor>(),
         );
 
-        Self { rgb, cube }
+        Self { cube }
     }
 }
 
 impl ServerState {
     fn update(&mut self, io: &mut EngineIo, query: &mut QueryResult) {
         if let Some(ChangeColor { rgb }) = io.inbox_first() {
-            dbg!(rgb);
-            self.rgb = rgb;
-        }
-
-        for key in query.iter() {
-            let mut extra = [0.; 4 * 4];
-            extra[..3].copy_from_slice(&self.rgb);
-            extra[3] = 1.;
-            io.add_component(key.entity(), &RenderExtra(extra));
+            for key in query.iter() {
+                let mut extra = [0.; 4 * 4];
+                extra[..3].copy_from_slice(&rgb);
+                extra[3] = 1.;
+                io.add_component(key.entity(), &RenderExtra(extra));
+            }
         }
     }
 }
---
+++ tools/compile_docker.sh
@@ -0,0 +1,1 @@
+docker run --rm -it -w /cwd -v $PWD:/cwd rust cargo build --bin cimvr_server --release
