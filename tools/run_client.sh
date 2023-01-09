3d6b9d1ea476198f36c925a7546e158059a8975b
Masterchef365 <duncan.freeman1@gmail.com>
2023-01-08 17:26:12 -0800

    Fix build issue with openxr, ew

--- tools/run_client.sh
+++ tools/run_client.sh
@@ -1,2 +1,3 @@
-base="./target/wasm32-unknown-unknown/release"
-cargo run --release --bin cimvr_client -- --vr $base/plugin*.wasm 
+pushd client
+base="../target/wasm32-unknown-unknown/release"
+cargo run --release --bin cimvr_client -- $base/plugin*.wasm 
