e7f7133e2b60585752ac16b3fbf7d94abb14dbd9
Masterchef365 <duncan.freeman1@gmail.com>
2023-03-14 02:47:24 -0700

    Subscribe to componentschema messages

--- client/src/component_ui.rs
+++ client/src/component_ui.rs
@@ -1,2 +1,22 @@
+use cimvr_engine::{Engine, interface::ComponentSchema};
+use egui::Context;
+
 pub struct ComponentUi {
+}
+
+impl ComponentUi {
+    pub fn new(engine: &mut Engine) -> Self {
+        engine.subscribe::<ComponentSchema>();
+        Self {
+        }
+    }
+
+    pub fn run(&mut self, ctx: &Context, engine: &mut Engine) {
+    }
+
+    pub fn update(&mut self, engine: &mut Engine) {
+        for msg in engine.inbox::<ComponentSchema>() {
+            dbg!(msg);
+        }
+    }
 }
--- client/src/ui.rs
+++ client/src/ui.rs
@@ -4,24 +4,28 @@ use cimvr_common::ui::*;
 use cimvr_engine::Engine;
 use egui::{color_picker::color_edit_button_rgb, Context, DragValue, ScrollArea, TextEdit, Ui};
 
-use crate::plugin_ui::PluginUi;
+use crate::{component_ui::ComponentUi, plugin_ui::PluginUi};
 
 pub struct OverlayUi {
     plugin_ui: PluginUi,
+    component_ui: ComponentUi,
 }
 
 impl OverlayUi {
     pub fn new(engine: &mut Engine) -> Self {
         Self {
             plugin_ui: PluginUi::new(engine),
+            component_ui: ComponentUi::new(engine),
         }
     }
 
     pub fn run(&mut self, ctx: &Context, engine: &mut Engine) {
         self.plugin_ui.run(ctx, engine);
+        self.component_ui.run(ctx, engine);
     }
 
     pub fn update(&mut self, engine: &mut Engine) {
-        self.plugin_ui.update(engine)
+        self.plugin_ui.update(engine);
+        self.component_ui.update(engine);
     }
 }
