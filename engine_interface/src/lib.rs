3d1e29b67bf7a092e6b1cd4488d1c8c4921d8d20
Masterchef365 <duncan.freeman1@gmail.com>
2023-03-14 01:18:45 -0700

    Simplify component_size_cached

--- engine_interface/src/lib.rs
+++ engine_interface/src/lib.rs
@@ -104,32 +104,19 @@ fn validate_component<C: Component>(c: &C) {
     }
 }
 
-/// Component size cache
-pub(crate) struct SizeCache(HashMap<&'static str, usize>);
-
-thread_local! {
-    /// Thread local component size cache
-    static SIZE_CACHE: RefCell<Lazy<SizeCache>> = RefCell::new(Lazy::new(|| SizeCache::new()));
-}
-
-impl SizeCache {
-    pub fn new() -> Self {
-        SizeCache(HashMap::new())
-    }
-
-    /// Get the size in bytse of the given component
-    #[track_caller]
-    pub fn size<C: Component>(&mut self) -> usize {
-        let SizeCache(map) = self;
-        *map.entry(C::ID)
-            .or_insert_with(|| max_component_size::<C>())
-    }
-}
-
 /// Get the size of a component
 #[track_caller]
 pub fn component_size_cached<C: Component>() -> usize {
-    SIZE_CACHE.with(|cache| cache.borrow_mut().size::<C>())
+    thread_local! {
+        /// Thread local component size cache
+        static SIZE_CACHE: RefCell<Lazy<HashMap<&'static str, usize>>> = RefCell::new(Lazy::new(|| HashMap::new()));
+    }
+    SIZE_CACHE.with(|cache| {
+        *cache
+            .borrow_mut()
+            .entry(C::ID)
+            .or_insert_with(|| max_component_size::<C>())
+    })
 }
 
 /// Get the ComponentId of a Component
