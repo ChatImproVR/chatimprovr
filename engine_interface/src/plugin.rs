eb75290cb190733933062478719fe0e04ac3aa17
Masterchef365 <duncan.freeman1@gmail.com>
2023-03-17 14:32:17 -0700

    Don't require reference for io.add_component()

--- engine_interface/src/plugin.rs
+++ engine_interface/src/plugin.rs
@@ -252,8 +252,8 @@ impl EngineIo {
     /// You may also use this to update existing component data, but it's better to write to the
     /// query for large batches instead
     #[track_caller]
-    pub fn add_component<C: Component>(&mut self, entity: EntityId, data: &C) {
-        let data = serialize(data).expect("Failed to serialize component data");
+    pub fn add_component<C: Component>(&mut self, entity: EntityId, data: C) {
+        let data = serialize(&data).expect("Failed to serialize component data");
 
         self.commands
             .push(EcsCommand::AddComponent(entity, component_id::<C>(), data));
