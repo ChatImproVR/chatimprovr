e1fdde3bc0a51d91c50ab26a8b1956e48c7d2aa7
exiled1 <rudyperalta831@gmail.com>
2023-03-17 21:55:54 -0700

    Checkpoint for mouse events

--- common/src/utils/input_helper.rs
+++ common/src/utils/input_helper.rs
@@ -1,22 +1,27 @@
-use std::collections::HashSet;
 use cimvr_engine_interface::prelude::EngineIo;
+use std::collections::HashSet;
 
-use crate::desktop::{KeyCode, ModifiersState, MouseButton, InputEvent, KeyboardEvent, ElementState};
+use crate::desktop::{
+    ElementState, InputEvent, KeyCode, KeyboardEvent, ModifiersState, MouseButton,
+};
 
 /// A helper struct for handling input events. This is a wrapper around the `InputEvent` enum.
 #[derive(Debug, Default, Clone, PartialEq)]
 struct InputHelper {
-    pub keys: HashSet<KeyCode>,
-    pub pressed_modifiers: ModifiersState,
-    pub mouse_state: MouseState
+    /// Keeps track of keys that are currently being pressed. A pressed key will be in this set.
+    pub pressed_keys: HashSet<KeyCode>,
+    /// Modifiers don't keep track of state, going up and down is treated like a boolean (on/off).
+    pub modifiers_state: ModifiersState,
+    pub mouse_state: MouseState,
 }
 
 #[derive(Debug, Default, Clone, PartialEq)]
 struct MouseState {
     pub position: (f32, f32),
     pub scroll: (f32, f32),
-    pub buttons: HashSet<MouseButton>,
-    // pub modifiers: ModifiersState,
+    pub buttons: HashSet<MouseButton>, // When handling the events, we can check for the element
+    // We don't
+    // state since that's an input event.
     pub in_window: bool,
 }
 
@@ -30,15 +35,14 @@ pub struct MouseButtonState {
 impl InputHelper {
     pub fn new() -> Self {
         Self {
-            keys: HashSet::new(),
-            pressed_modifiers: ModifiersState::default(),
+            pressed_keys: HashSet::new(),
+            modifiers_state: ModifiersState::default(),
             mouse_state: MouseState {
                 position: (0.0, 0.0),
                 scroll: (0.0, 0.0),
                 buttons: HashSet::new(),
-                // modifiers: ModifiersState::default(),
                 in_window: false,
-            }
+            },
         }
     }
 
@@ -47,28 +51,26 @@ impl InputHelper {
             self.update(&event);
         }
     }
-    
-    fn update(&mut self, input_event: &InputEvent){
+
+    fn update(&mut self, input_event: &InputEvent) {
         match input_event {
             InputEvent::Keyboard(keyboard_event) => self.handle_keyboard_event(keyboard_event),
             _ => {}
         }
     }
 
-    fn handle_keyboard_event(&mut self, keyboard_event: &KeyboardEvent){
+    fn handle_keyboard_event(&mut self, keyboard_event: &KeyboardEvent) {
         match keyboard_event {
-            KeyboardEvent::Key { key, state } => {
-                match state {
-                    ElementState::Pressed => {
-                        self.keys.insert(*key);
-                    }
-                    ElementState::Released => {
-                        self.keys.remove(key);
-                    }
+            KeyboardEvent::Key { key, state } => match state {
+                ElementState::Pressed => {
+                    self.pressed_keys.insert(*key);
                 }
-            }
+                ElementState::Released => {
+                    self.pressed_keys.remove(key);
+                }
+            },
             KeyboardEvent::Modifiers(modifiers_state) => {
-                self.pressed_modifiers = *modifiers_state;
+                self.modifiers_state = *modifiers_state;
             }
         }
     }
