797ab981a0e1a7b00325181eb80c18fcb4b445e0
Masterchef365 <duncan.freeman1@gmail.com>
2023-03-22 21:54:53 -0700

    Increase tick rate, set registry to sparse in docker script

--- tools/compile_docker.sh
+++ tools/compile_docker.sh
@@ -1,1 +1,7 @@
-docker run --rm -it -w /cwd -v $PWD:/cwd rust cargo build --bin cimvr_server --release
+docker run \
+    --env CARGO_REGISTRIES_CRATES_IO_PROTOCOL=sparse \
+    --rm \
+    -it \
+    -w /cwd \
+    -v $PWD:/cwd \
+    rust cargo build --bin cimvr_server --release
