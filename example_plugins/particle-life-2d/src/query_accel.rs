58bb080097eae836f5f7807156a944379e801b3a
Masterchef365 <duncan.freeman1@gmail.com>
2023-03-29 17:52:19 -0700

    Split between 2d and 3d particle life

---
+++ example_plugins/particle-life-2d/src/query_accel.rs
@@ -0,0 +1,102 @@
+use cimvr_common::glam::Vec2;
+use zwohash::HashMap;
+
+/// Euclidean neighborhood query accelerator. Uses a hashmap grid.
+pub struct QueryAccelerator {
+    cells: HashMap<[i32; 2], Vec<usize>>,
+    neighbors: Vec<[i32; 2]>,
+    radius: f32,
+    radius_sq: f32,
+}
+
+impl QueryAccelerator {
+    /// Construct a new query accelerator
+    pub fn new(points: &[Vec2], radius: f32) -> Self {
+        let mut cells: HashMap<[i32; 2], Vec<usize>> = HashMap::default();
+
+        for (idx, &point) in points.iter().enumerate() {
+            cells.entry(quantize(point, radius)).or_default().push(idx);
+        }
+
+        let neighbors = neighborhood::<2>();
+
+        Self {
+            cells,
+            radius,
+            radius_sq: radius * radius,
+            neighbors,
+        }
+    }
+
+    /*
+    /// This should result in better cache locality for queries, but may take some time.
+    pub fn sort_indices(mut self) -> Self {
+        for indices in self.cells.values_mut() {
+            indices.sort();
+        }
+        self
+    }
+    */
+
+    // Query the neighbors of `queried_idx` in `points`
+    pub fn query_neighbors<'s, 'p: 's>(
+        &'s self,
+        points: &'p [Vec2],
+        queried_idx: usize,
+    ) -> impl Iterator<Item = usize> + 's {
+        let query_point = points[queried_idx];
+        let origin = quantize(query_point, self.radius);
+
+        self.neighbors
+            .iter()
+            .map(move |diff| {
+                let key = add(origin, *diff);
+                self.cells.get(&key).map(|cell_indices| {
+                    cell_indices.iter().copied().filter(move |&idx| {
+                        let dist = (points[idx] - query_point).length_squared();
+                        idx != queried_idx && dist <= self.radius_sq
+                    })
+                })
+            })
+            .flatten()
+            .flatten()
+    }
+
+    /*
+    pub fn tiles(&self) -> impl Iterator<Item = (&[i32; 2], &Vec<usize>)> {
+        self.cells.iter()
+    }
+    */
+}
+
+fn add(mut a: [i32; 2], b: [i32; 2]) -> [i32; 2] {
+    a.iter_mut().zip(b).for_each(|(a, b)| *a += b);
+    a
+}
+
+fn quantize(p: Vec2, radius: f32) -> [i32; 2] {
+    (*p.as_ref()).map(|v| (v / radius).floor() as i32)
+}
+
+fn neighborhood<const N: usize>() -> Vec<[i32; N]> {
+    combos(-1, 1, 1)
+}
+
+fn combos<const N: usize>(min: i32, max: i32, step: i32) -> Vec<[i32; N]> {
+    let mut dims = [min; N];
+    let mut combos = vec![];
+    loop {
+        combos.push(dims);
+        if dims == [max; N] {
+            break combos;
+        }
+        for i in 0..dims.len() {
+            if dims[i] < max {
+                dims[i] += step;
+                break;
+            } else {
+                dims[i] = min;
+            }
+        }
+    }
+}
---
+++ example_plugins/particle-life-2d/src/sim.rs
@@ -0,0 +1,178 @@
+use cimvr_common::glam::Vec2;
+use cimvr_engine_interface::pcg::Pcg;
+
+use crate::query_accel::QueryAccelerator;
+
+pub struct SimState {
+    particles: Vec<Particle>,
+    config: SimConfig,
+    max_interaction_radius: f32,
+}
+
+type Color = u8;
+
+#[derive(Clone, Copy)]
+pub struct Particle {
+    pub pos: Vec2,
+    pub vel: Vec2,
+    pub color: Color,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct Behaviour {
+    /// Magnitude of the default repulsion force
+    pub default_repulse: f32,
+    /// Zero point between default repulsion and particle interaction (0 to 1)
+    pub inter_threshold: f32,
+    /// Interaction peak strength
+    pub inter_strength: f32,
+    /// Maximum distance of particle interaction (0 to 1)
+    pub inter_max_dist: f32,
+}
+
+/// Display colors and physical behaviour coefficients
+#[derive(Clone, Debug)]
+pub struct SimConfig {
+    pub colors: Vec<[f32; 3]>,
+    pub behaviours: Vec<Behaviour>,
+    pub damping: f32,
+}
+
+impl Behaviour {
+    /// Returns the force on this particle
+    ///
+    /// Distance is in the range `0.0..=1.0`
+    fn interact(&self, dist: f32) -> f32 {
+        if dist < self.inter_threshold {
+            let f = dist / self.inter_threshold;
+            (1. - f) * -self.default_repulse
+        } else if dist > self.inter_max_dist {
+            0.0
+        } else {
+            let x = dist - self.inter_threshold;
+            let x = x / (self.inter_max_dist - self.inter_threshold);
+            let x = x * 2. - 1.;
+            let x = 1. - x.abs();
+            x * self.inter_strength
+        }
+    }
+}
+
+impl SimState {
+    pub fn new(rng: &mut Pcg, config: SimConfig, n: usize) -> Self {
+        let particles = (0..n).map(|_| random_particle(rng, &config)).collect();
+        let max_interaction_radius: f32 = config
+            .behaviours
+            .iter()
+            .map(|b| b.inter_max_dist)
+            .fold(0., |r, acc| acc.max(r));
+
+        Self {
+            particles,
+            config,
+            max_interaction_radius,
+        }
+    }
+
+    pub fn step(&mut self, dt: f32) {
+        let points: Vec<Vec2> = self.particles.iter().map(|p| p.pos).collect();
+        let accel = QueryAccelerator::new(&points, self.max_interaction_radius);
+
+        let len = self.particles.len();
+        for i in 0..len {
+            let mut total_accel = Vec2::ZERO;
+            for neighbor in accel.query_neighbors(&points, i) {
+                let a = self.particles[i];
+                let b = self.particles[neighbor];
+
+                // The vector pointing from a to b
+                let diff = b.pos - a.pos;
+
+                // Distance is capped
+                let dist = diff.length();
+
+                // Accelerate towards b
+                let normal = diff.normalize();
+                let behav = self.config.get_bahaviour(a.color, b.color);
+                let accel = normal * behav.interact(dist) / dist;
+                total_accel += accel;
+            }
+
+            let vel = self.particles[i].vel + total_accel * dt;
+
+            // Dampen velocity
+            let vel = vel * (1. - dt * self.config.damping);
+
+            self.particles[i].vel = vel;
+            self.particles[i].pos += vel * dt;
+        }
+    }
+
+    pub fn particles(&self) -> &[Particle] {
+        &self.particles
+    }
+
+    pub fn config(&self) -> &SimConfig {
+        &self.config
+    }
+}
+
+impl SimConfig {
+    fn random_color(&self, rng: &mut Pcg) -> Color {
+        (rng.gen_u32() as usize % self.colors.len()) as u8
+    }
+
+    pub fn get_bahaviour(&self, a: Color, b: Color) -> Behaviour {
+        let idx = a as usize * self.colors.len() + b as usize;
+        self.behaviours[idx]
+    }
+}
+
+fn random_particle(rng: &mut Pcg, config: &SimConfig) -> Particle {
+    let range = 5.;
+    Particle {
+        pos: Vec2::new(rng.gen_f32(), rng.gen_f32()) * range - Vec2::new(range / 2., range / 2.),
+        vel: Vec2::ZERO,
+        color: config.random_color(rng),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_behaviour() {
+        let behav = Behaviour {
+            default_repulse: 1.0,
+            inter_threshold: 0.25,
+            inter_strength: 3.0,
+            inter_max_dist: 0.75,
+        };
+
+        assert_eq!(behav.interact(0.), -behav.default_repulse);
+        assert_eq!(behav.interact(behav.inter_threshold), 0.0);
+        assert_eq!(behav.interact(0.25 + 0.125), behav.inter_strength / 2.);
+        assert_eq!(behav.interact(0.5), behav.inter_strength);
+        assert_eq!(behav.interact(behav.inter_max_dist), 0.0);
+        assert_eq!(behav.interact(0.85), 0.0);
+    }
+}
+
+impl Default for Behaviour {
+    fn default() -> Self {
+        Self {
+            default_repulse: 10.,
+            inter_threshold: 0.02,
+            inter_strength: 1.,
+            inter_max_dist: 0.2,
+        }
+    }
+}
+
+impl Behaviour {
+    pub fn with_inter_strength(mut self, inter_strength: f32) -> Self {
+        self.inter_strength = inter_strength;
+        self
+    }
+}
