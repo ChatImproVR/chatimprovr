6da7766416ab882c295872e85898140e39eecd2c
Grace Todd <56061990+toddgr@users.noreply.github.com>
2023-04-07 10:20:44 -0700

    missing .build for adding components

--- example_plugins/obj_parser/src/lib.rs
+++ example_plugins/obj_parser/src/lib.rs
@@ -34,11 +34,11 @@ struct ServerState;
 impl UserState for ServerState {
     // Implement a constructor
     fn new(io: &mut EngineIo, _sched: &mut EngineSchedule<Self>) -> Self {
-        let ent = io.create_entity();
+        let ent = io.create_entity().build();
         io.add_component(ent, &Transform::identity());
         io.add_component(ent, &Render::new(SHIP_RDR).primitive(Primitive::Triangles));
         io.add_component(ent, &Synchronized);
-
+        
         println!("Hello, server!");
         Self
     }
