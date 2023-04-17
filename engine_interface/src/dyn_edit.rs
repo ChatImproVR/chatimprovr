59afad13cfedf1ef5d38bcf1897bbbf6b4b2d40e
Masterchef365 <duncan.freeman1@gmail.com>
2023-04-17 02:05:30 -0700

    Work on enabling component schema download

--- engine_interface/src/dyn_edit.rs
+++ engine_interface/src/dyn_edit.rs
@@ -7,11 +7,13 @@ use serde::{Deserialize, Serialize};
 // TODO: Add metadata for edit requests, so that servers can intelligently filter...
 
 /// A dynamic edit request sent to the server
+/// Emitted by e.g. the component GUI to change entities containing the Synchronized component.
 #[derive(Message, Serialize, Deserialize, Clone)]
 #[locality("Remote")]
 pub struct DynamicEditRequest(pub DynamicEdit);
 
 /// A dynamic edit command sent to the engine
+/// Emitted by e.g. the component GUI to locally change entities
 #[derive(Message, Serialize, Deserialize, Clone)]
 #[locality("Local")]
 pub struct DynamicEditCommand(pub DynamicEdit);
@@ -21,6 +23,8 @@ pub struct DynamicEditCommand(pub DynamicEdit);
 pub struct DynamicEdit {
     /// Target entity
     pub entity: EntityId,
+    // TODO: This is a potentially harmful thing to expose.
+    // We should provide a wrapper around it!
     /// Full component data state of this entity
     pub components: HashMap<ComponentId, Vec<u8>>,
 }
--- example_plugins/dyn_edit_perms/src/lib.rs
+++ example_plugins/dyn_edit_perms/src/lib.rs
@@ -1,10 +1,24 @@
 use cimvr_engine_interface::{
+    dbg,
     dyn_edit::{DynamicEditCommand, DynamicEditRequest},
-    make_app_state,
+    make_app_state, pkg_namespace,
     prelude::*,
+    println, ComponentSchema,
 };
+use serde::{Deserialize, Serialize};
 
 struct ServerState;
+
+impl UserState for ServerState {
+    fn new(_io: &mut EngineIo, sched: &mut EngineSchedule<Self>) -> Self {
+        sched
+            .add_system(Self::update)
+            .subscribe::<DynamicEditRequest>()
+            .subscribe::<ComponentSchema>()
+            .build();
+        Self
+    }
+}
 
 impl ServerState {
     fn update(&mut self, io: &mut EngineIo, _query: &mut QueryResult) {
@@ -13,17 +27,41 @@ impl ServerState {
         for DynamicEditRequest(edit) in io.inbox().collect::<Vec<_>>() {
             io.send(&DynamicEditCommand(edit));
         }
+
+        // The second purpose of this plugin:
+        // Receive component schema server-side, and forward them
+        // client-side for display/editing
+        for component_schema in io.inbox::<ComponentSchema>().collect::<Vec<_>>() {
+            io.send(&ComponentSchemaDownload(component_schema));
+        }
     }
 }
 
-impl UserState for ServerState {
+#[derive(Message, Serialize, Deserialize, Debug)]
+#[locality("Remote")]
+struct ComponentSchemaDownload(ComponentSchema);
+
+struct ClientState;
+
+impl UserState for ClientState {
     fn new(_io: &mut EngineIo, sched: &mut EngineSchedule<Self>) -> Self {
         sched
             .add_system(Self::update)
-            .subscribe::<DynamicEditRequest>()
+            .subscribe::<ComponentSchemaDownload>()
             .build();
         Self
     }
 }
 
-make_app_state!(DummyUserState, ServerState);
+impl ClientState {
+    fn update(&mut self, io: &mut EngineIo, _query: &mut QueryResult) {
+        // Download component schema data from server and make it available client-side.
+        for ComponentSchemaDownload(component_schema) in
+            io.inbox::<ComponentSchemaDownload>().collect::<Vec<_>>()
+        {
+            io.send(&component_schema);
+        }
+    }
+}
+
+make_app_state!(ClientState, ServerState);
