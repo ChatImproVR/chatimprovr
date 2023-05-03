2bb3232a11a80c2c825f1f27e83eb9cb565798b6
Masterchef365 <duncan.freeman1@gmail.com>
2023-05-03 00:20:33 -0700

    Fix parenting and demo it

--- example_plugins/parenting/src/lib.rs
+++ example_plugins/parenting/src/lib.rs
@@ -5,12 +5,13 @@ use serde::{Deserialize, Serialize};
 struct ClientState;
 struct ServerState;
 
+#[cfg(not(feature = "library"))]
 make_app_state!(ClientState, ServerState);
 
 /// Sets our Transform to that of the given Entity,
-/// prepending it with the Transform in this Component.
+/// appending it with the Transform in this Component.
 #[derive(Component, Serialize, Deserialize, Clone, Copy, Debug)]
-pub struct Parent(EntityId, Transform);
+pub struct ChildOf(pub EntityId, pub Transform);
 
 fn add_systems<State>(sched: &mut EngineSchedule<State>, f: Callback<State>) {
     sched
@@ -23,7 +24,7 @@ fn add_systems<State>(sched: &mut EngineSchedule<State>, f: Callback<State>) {
             "Objects With Parents",
             Query::new()
                 .intersect::<Transform>(Access::Write)
-                .intersect::<Parent>(Access::Read),
+                .intersect::<ChildOf>(Access::Read),
         )
         .build();
 }
@@ -31,7 +32,7 @@ fn add_systems<State>(sched: &mut EngineSchedule<State>, f: Callback<State>) {
 fn handle_parenting(query: &mut QueryResult) {
     // TODO: Determine the dependency graph for real; avoid loops!
     for child_id in query.iter("Objects With Parents") {
-        let Parent(parent_id, prepend_tf) = query.read(child_id);
+        let ChildOf(parent_id, append_tf) = query.read(child_id);
         if parent_id == child_id {
             println!(
                 "Entity {:?} is parented to itself! Alabama would be proud.",
@@ -41,8 +42,8 @@ fn handle_parenting(query: &mut QueryResult) {
         }
 
         if query.has_component::<Transform>(parent_id) {
-            let parent_tf = query.read(parent_id);
-            query.write(child_id, &(prepend_tf * parent_tf));
+            let parent_tf: Transform = query.read(parent_id);
+            query.write(child_id, &(parent_tf * append_tf));
         }
     }
 }
@@ -73,7 +74,7 @@ impl ClientState {
     }
 }
 
-impl Default for Parent {
+impl Default for ChildOf {
     fn default() -> Self {
         Self(
             EntityId(0xBAD_BAD_BAD_BAD_BAD_BAD_BAD_BAD_BAD_BAD),
