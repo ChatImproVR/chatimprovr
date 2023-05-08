f4cdda57abf48c5018eb82b76389a54bd88264f5
Masterchef365 <duncan.freeman1@gmail.com>
2023-05-08 01:03:07 -0700

    More ideas

--- common/src/gpgpu.rs
+++ common/src/gpgpu.rs
@@ -13,9 +13,9 @@ use crate::{make_handle, render::MeshHandle, GenericHandle};
 pub struct ComputeShader(GenericHandle);
 make_handle!(ComputeShader);
 
-#[derive(Serialize, Deserialize, Default, Copy, Clone, Debug, Hash, PartialEq, Eq)]
-pub struct GpuBuffer {
-    pub id: GenericHandle,
+#[derive(Serialize, Deserialize, Copy, Clone, Debug, Hash, PartialEq, Eq)]
+pub struct GpuBufferHandle {
+    pub handle: GenericHandle,
     pub typ: BufferType,
     pub layout: BufferLayout,
 }
@@ -26,16 +26,17 @@ pub enum BufferType {
     Uniform,
 }
 
-fn upload_buffer(io: &mut EngineIo, typ: BufferType, layout: BufferLayout, n: usize, bytes: &[u8]) {
-}
-
 #[derive(Serialize, Deserialize, Copy, Clone, Debug, Hash, PartialEq, Eq)]
 pub enum BufferLayout {
-    Float,
-    Vec2,
+    Float(usize),
+    Vec2(usize),
     //No vec3, because it's inefficient on most hardware! Just use vec4.
-    Vec4,
-    Vertex,
+    Vec4(usize),
+    /// RGBA 2D floating point image with dimensions of (width, height)
+    // /// Note that each image is ALSO supplied as a sample-able texture!
+    Image2D(usize, usize),
+    //Image3D,
+    //Vertex,
     //Int,
     //IVec2,
     //Ivec4,
@@ -45,10 +46,21 @@ pub enum BufferLayout {
     //Custom(CustomBufferLayout) ( Soon™ )
 }
 
+impl BufferLayout {
+    pub fn size(&self) -> usize {
+        match self {
+            BufferLayout::Float(n) => 4 * n,
+            BufferLayout::Vec2(n) => 4 * 2 * n,
+            BufferLayout::Vec4(n) => 4 * 4 * n,
+            BufferLayout::Image2D(w, h) => 4 * 4 * w * h,
+        }
+    }
+}
+
 /// A buffer and it's associated data
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct BufferPacket {
-    pub id: GpuBuffer,
+    pub handle: GpuBufferHandle,
     pub data: Vec<u8>,
 }
 
@@ -63,15 +75,47 @@ pub struct GpuBufferUpload(BufferPacket);
 pub struct GpuBufferDownload(BufferPacket);
 
 /// GPGPU compute operation
-#[derive(Serialize, Deserialize, Copy, Debug, Clone)]
-enum ComputeOperation {
-    InvokeComputeShader(ComputeShader, i32, i32, i32),
-    CopyToVertices(GpuBuffer, MeshHandle),
-    CopyToIndices(GpuBuffer, MeshHandle),
+#[derive(Serialize, Deserialize, Debug, Clone)]
+enum GpuComputeOperation {
+    InvokeComputeShader {
+        shader: ComputeShader,
+        x: i32,
+        y: i32,
+        z: i32,
+        buffers: Vec<GpuBufferHandle>,
+    },
+    /// Executes a builtin shader which copies consecutive pairs of vec4s from the source buffer
+    /// into the color and uv fields of the vertices of the referenced mesh
+    /// So, if you have a mesh with N vertices, N*2 Vec4s.
+    CopyToVertices(GpuBufferHandle, MeshHandle),
+    //CopyToIndices(GpuBuffer, MeshHandle),
 }
 
 #[derive(Message, Serialize, Deserialize, Debug, Clone)]
 #[locality("Local")]
 pub struct GpuDeclarePipeline {
     stages: Vec<ComputeShader>,
+}
+
+impl BufferPacket {
+    pub fn new(handle: GpuBufferHandle, data: Vec<u8>) -> Self {
+        Self { handle, data }
+    }
+}
+
+impl GpuBufferHandle {
+    pub const fn new(name: &str, typ: BufferType, layout: BufferLayout) -> Self {
+        let handle = GenericHandle::new(name);
+        Self {
+            handle,
+            typ,
+            layout,
+        }
+    }
+
+    pub fn index(self, i: u128) -> Self {
+        let mut out = self;
+        out.handle = out.handle.index(i);
+        out
+    }
 }
