33e4154a216f155e7b01f9889490376e8e58af14
Masterchef365 <duncan.freeman1@gmail.com>
2023-07-08 01:36:09 -0700

    My hands are t yping words

--- client/src/desktop.rs
+++ client/src/desktop.rs
@@ -11,6 +11,7 @@ use glutin::event::{Event, WindowEvent};
 use glutin::event_loop::ControlFlow;
 use std::path::PathBuf;
 use std::sync::Arc;
+use std::time::{Instant, Duration};
 
 pub fn mainloop(mut args: Opt) -> Result<()> {
     let options = eframe::NativeOptions {
@@ -20,33 +21,33 @@ pub fn mainloop(mut args: Opt) -> Result<()> {
         ..Default::default()
     };
     eframe::run_native(
-        "Custom 3D painting in eframe using glow",
+        "ChatImproVR",
         options,
-        Box::new(|cc| Box::new(MyApp::new(cc))),
+        Box::new(|cc| Box::new(ChatimprovrEframeApp::new(cc, args).unwrap())),
     )
     .map_err(|e| format_err!("{:#}", e))
 }
 
-struct MyApp {
+struct ChatimprovrEframeApp {
     /// Behind an `Arc<Mutex<…>>` so we can pass it to [`egui::PaintCallback`] and paint later.
-    rotating_triangle: Arc<Mutex<RotatingTriangle>>,
-    angle: f32,
+    cimvr_widget: Arc<Mutex<ChatimprovrWidget>>,
+    login_screen: LoginScreen,
 }
 
-impl MyApp {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+impl ChatimprovrEframeApp {
+    fn new(cc: &eframe::CreationContext<'_>, args: Opt) -> Result<Self> {
         let gl = cc
             .gl
-            .as_ref()
+            .clone()
             .expect("You need to run eframe with the glow backend");
-        Self {
-            rotating_triangle: Arc::new(Mutex::new(RotatingTriangle::new(gl))),
-            angle: 0.0,
-        }
+        Ok(Self {
+            login_screen: LoginScreen::new(args.clone())?,
+            cimvr_widget: Arc::new(Mutex::new(ChatimprovrWidget::new(gl, args)?)),
+        })
     }
 }
 
-impl eframe::App for MyApp {
+impl eframe::App for ChatimprovrEframeApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.horizontal(|ui| {
@@ -65,293 +66,203 @@ impl eframe::App for MyApp {
 
     fn on_exit(&mut self, gl: Option<&glow::Context>) {
         if let Some(gl) = gl {
-            self.rotating_triangle.lock().destroy(gl);
+            self.cimvr_widget.lock().destroy(gl);
         }
     }
 }
 
-impl MyApp {
+impl ChatimprovrEframeApp {
     fn custom_painting(&mut self, ui: &mut egui::Ui) {
         let (rect, response) =
             ui.allocate_exact_size(egui::Vec2::splat(300.0), egui::Sense::drag());
 
-        self.angle += response.drag_delta().x * 0.01;
+        // We're a game, renfer once per frame 
+        ui.ctx().request_repaint();
 
         // Clone locals so we can move them into the paint callback:
-        let angle = self.angle;
-        let rotating_triangle = self.rotating_triangle.clone();
+        let widge = self.cimvr_widget.clone();
 
         let callback = egui::PaintCallback {
             rect,
             callback: std::sync::Arc::new(egui_glow::CallbackFn::new(move |_info, painter| {
-                rotating_triangle.lock().paint(painter.gl(), angle);
+                widge.lock().paint(painter.gl());
             })),
         };
         ui.painter().add(callback);
     }
 }
 
-struct RotatingTriangle {
-    program: glow::Program,
-    vertex_array: glow::VertexArray,
+struct ChatimprovrWidget {
+    input: DesktopInputHandler,
+    window_control: Option<WindowController>,
+    client: Option<Client>,
 }
 
-impl RotatingTriangle {
-    fn new(gl: &glow::Context) -> Self {
-        use glow::HasContext as _;
-
-        let shader_version = if cfg!(target_arch = "wasm32") {
-            "#version 300 es"
-        } else {
-            "#version 330"
-        };
-
-        unsafe {
-            let program = gl.create_program().expect("Cannot create program");
-
-            let (vertex_shader_source, fragment_shader_source) = (
-                r#"
-                    const vec2 verts[3] = vec2[3](
-                        vec2(0.0, 1.0),
-                        vec2(-1.0, -1.0),
-                        vec2(1.0, -1.0)
-                    );
-                    const vec4 colors[3] = vec4[3](
-                        vec4(1.0, 0.0, 0.0, 1.0),
-                        vec4(0.0, 1.0, 0.0, 1.0),
-                        vec4(0.0, 0.0, 1.0, 1.0)
-                    );
-                    out vec4 v_color;
-                    uniform float u_angle;
-                    void main() {
-                        v_color = colors[gl_VertexID];
-                        gl_Position = vec4(verts[gl_VertexID], 0.0, 1.0);
-                        gl_Position.x *= cos(u_angle);
-                    }
-                "#,
-                r#"
-                    precision mediump float;
-                    in vec4 v_color;
-                    out vec4 out_color;
-                    void main() {
-                        out_color = v_color;
-                    }
-                "#,
-            );
-
-            let shader_sources = [
-                (glow::VERTEX_SHADER, vertex_shader_source),
-                (glow::FRAGMENT_SHADER, fragment_shader_source),
-            ];
-
-            let shaders: Vec<_> = shader_sources
-                .iter()
-                .map(|(shader_type, shader_source)| {
-                    let shader = gl
-                        .create_shader(*shader_type)
-                        .expect("Cannot create shader");
-                    gl.shader_source(shader, &format!("{}\n{}", shader_version, shader_source));
-                    gl.compile_shader(shader);
-                    assert!(
-                        gl.get_shader_compile_status(shader),
-                        "Failed to compile {shader_type}: {}",
-                        gl.get_shader_info_log(shader)
-                    );
-                    gl.attach_shader(program, shader);
-                    shader
-                })
-                .collect();
-
-            gl.link_program(program);
-            assert!(
-                gl.get_program_link_status(program),
-                "{}",
-                gl.get_program_info_log(program)
-            );
-
-            for shader in shaders {
-                gl.detach_shader(program, shader);
-                gl.delete_shader(shader);
-            }
-
-            let vertex_array = gl
-                .create_vertex_array()
-                .expect("Cannot create vertex array");
-
-            Self {
-                program,
-                vertex_array,
-            }
-        }
+impl ChatimprovrWidget {
+    fn new(gl: Arc<glow::Context>, mut args: Opt) -> Result<Self> {
+        let client = Client::new(gl, args.login_info()?)?;
+        Ok(Self {
+            input: DesktopInputHandler::new(),
+            client: Some(client),
+            window_control: None,
+        })
     }
 
     fn destroy(&self, gl: &glow::Context) {
-        use glow::HasContext as _;
-        unsafe {
-            gl.delete_program(self.program);
-            gl.delete_vertex_array(self.vertex_array);
-        }
+        todo!()
     }
 
-    fn paint(&self, gl: &glow::Context, angle: f32) {
-        use glow::HasContext as _;
-        unsafe {
-            gl.use_program(Some(self.program));
-            gl.uniform_1_f32(
-                gl.get_uniform_location(self.program, "u_angle").as_ref(),
-                angle,
-            );
-            gl.bind_vertex_array(Some(self.vertex_array));
-            gl.draw_arrays(glow::TRIANGLES, 0, 3);
+    fn paint(&mut self, gl: &glow::Context) {
+        /*
+        // Login page
+        if client.is_none() {
+            // Attempt to login via command line arg
+            if args.connect.is_some() {
+                client = login_screen.login(&gl);
+                // Don't loop
+                args.connect = None;
+            }
+
+            // Otherwise, use the GUI to login
+            egui_glow.run(glutin_ctx.window(), |ctx| {
+                egui::CentralPanel::default().show(ctx, |ui| {
+                    if login_screen.show(ui) {
+                        client = login_screen.login(&gl);
+                    }
+                });
+            });
         }
+        */
+
+        if let Some(client) = &mut self.client {
+            if self.window_control.is_none() {
+                self.window_control = Some(WindowController::new(client.engine()));
+            }
+
+            // Download messages from server
+            client.download().expect("Message download");
+
+            // Send input history
+            self.input.get_history(client.engine());
+            let gamepad_state = client.gamepad.update();
+            client.engine().send(gamepad_state);
+
+            // Pre update stage
+            client
+                .engine()
+                .dispatch(Stage::PreUpdate)
+                .expect("Frame pre-update");
+
+            // Update stage
+            client
+                .engine()
+                .dispatch(Stage::Update)
+                .expect("Frame udpate");
+
+            /*
+            TODO: Re-implement window control
+            self.window_control
+                .get_or_insert_with(|| WindowController::new(client.engine()))
+                .update(client.engine(), glutin_ctx.window());
+
+            // Collect UI input
+            egui_glow.run(glutin_ctx.window(), |ctx| client.update_ui(ctx));
+            */
+
+            // Render frame
+            client
+                .render_frame(Mat4::IDENTITY, 0)
+                .expect("Frame render");
+            }
+
+        /*
+        TODO: Travel requests
+        // Check for travel requests
+        let travel_request = client.as_mut().and_then(|c| c.travel_request());
+
+        if let Some(client) = &mut client {
+            // Post update stage
+            client
+                .engine()
+                .dispatch(Stage::PostUpdate)
+                .expect("Frame post-update");
+
+            // Upload messages to server
+            client.upload().expect("Message upload");
+        }
+
+        // Check for travel requests
+        if let Some(travel_request) = travel_request {
+            login_screen.login_file.last_login_address = travel_request.address;
+            client = login_screen.login(&gl);
+        }
+        */
     }
 }
 
 /*
-pub fn old_mainloop(mut args: Opt) -> Result<()> {
-    // Set up window
-    let event_loop = glutin::event_loop::EventLoop::new();
-    let window_builder = glutin::window::WindowBuilder::new().with_title("ChatImproVR");
+   pub fn old_mainloop(mut args: Opt) -> Result<()> {
+// Set up window
+let event_loop = glutin::event_loop::EventLoop::new();
+let window_builder = glutin::window::WindowBuilder::new().with_title("ChatImproVR");
 
-    // Set up OpenGL
-    let glutin_ctx = unsafe {
-        glutin::ContextBuilder::new()
-            .with_vsync(true)
-            .build_windowed(window_builder, &event_loop)?
-            .make_current()
-            .unwrap()
-    };
+// Set up OpenGL
+let glutin_ctx = unsafe {
+glutin::ContextBuilder::new()
+.with_vsync(true)
+.build_windowed(window_builder, &event_loop)?
+.make_current()
+.unwrap()
+};
 
-    let gl = unsafe {
-        gl::Context::from_loader_function(|s| glutin_ctx.get_proc_address(s) as *const _)
-    };
-    let gl = Arc::new(gl);
+let gl = unsafe {
+gl::Context::from_loader_function(|s| glutin_ctx.get_proc_address(s) as *const _)
+};
+let gl = Arc::new(gl);
 
-    // Set up egui
-    let mut egui_glow = egui_glow::EguiGlow::new(&event_loop, gl.clone());
+// Set up egui
+let mut egui_glow = egui_glow::EguiGlow::new(&event_loop, gl.clone());
 
-    // Set up desktop input
-    let mut input = DesktopInputHandler::new();
-    let mut window_control = None;
+// Set up desktop input
+let mut input = DesktopInputHandler::new();
+let mut window_control = None;
 
-    // Setup client code
-    let mut client: Option<Client> = None;
-    let mut login_screen = LoginScreen::new(args.clone())?;
+// Setup client code
+let mut client: Option<Client> = None;
+let mut login_screen = LoginScreen::new(args.clone())?;
 
-    // Run event loop
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Poll;
-        match event {
-            Event::MainEventsCleared => {
-                glutin_ctx.window().request_redraw();
-            }
-            Event::RedrawRequested(_) => {
-                // Login page
-                if client.is_none() {
-                    // Attempt to login via command line arg
-                    if args.connect.is_some() {
-                        client = login_screen.login(&gl);
-                        // Don't loop
-                        args.connect = None;
-                    }
+// Run event loop
+event_loop.run(move |event, _, control_flow| {
+ *control_flow = ControlFlow::Poll;
+ match event {
+ Event::MainEventsCleared => {
+ glutin_ctx.window().request_redraw();
+ }
+ Event::RedrawRequested(_) => 
+ Event::WindowEvent { ref event, .. } => {
+ if !egui_glow.on_event(&event) {
+ input.handle_winit_event(event);
+ }
 
-                    // Otherwise, use the GUI to login
-                    egui_glow.run(glutin_ctx.window(), |ctx| {
-                        egui::CentralPanel::default().show(ctx, |ui| {
-                            if login_screen.show(ui) {
-                                client = login_screen.login(&gl);
-                            }
-                        });
-                    });
-                }
-
-                if let Some(client) = &mut client {
-                    if window_control.is_none() {
-                        window_control = Some(WindowController::new(client.engine()));
-                    }
-
-                    // Download messages from server
-                    client.download().expect("Message download");
-
-                    // Send input history
-                    input.get_history(client.engine());
-                    let gamepad_state = client.gamepad.update();
-                    client.engine().send(gamepad_state);
-
-                    // Pre update stage
-                    client
-                        .engine()
-                        .dispatch(Stage::PreUpdate)
-                        .expect("Frame pre-update");
-
-                    // Update stage
-                    client
-                        .engine()
-                        .dispatch(Stage::Update)
-                        .expect("Frame udpate");
-
-                    window_control
-                        .get_or_insert_with(|| WindowController::new(client.engine()))
-                        .update(client.engine(), glutin_ctx.window());
-
-                    // Collect UI input
-                    egui_glow.run(glutin_ctx.window(), |ctx| client.update_ui(ctx));
-
-                    // Render frame
-                    client
-                        .render_frame(Mat4::IDENTITY, 0)
-                        .expect("Frame render");
-                }
-
-                // Render UI
-                egui_glow.paint(glutin_ctx.window());
-
-                // Check for travel requests
-                let travel_request = client.as_mut().and_then(|c| c.travel_request());
-
-                if let Some(client) = &mut client {
-                    // Post update stage
-                    client
-                        .engine()
-                        .dispatch(Stage::PostUpdate)
-                        .expect("Frame post-update");
-
-                    // Upload messages to server
-                    client.upload().expect("Message upload");
-                }
-
-                glutin_ctx.swap_buffers().unwrap();
-
-                // Check for travel requests
-                if let Some(travel_request) = travel_request {
-                    login_screen.login_file.last_login_address = travel_request.address;
-                    client = login_screen.login(&gl);
-                }
-            }
-            Event::WindowEvent { ref event, .. } => {
-                if !egui_glow.on_event(&event) {
-                    input.handle_winit_event(event);
-                }
-
-                match event {
-                    WindowEvent::Resized(ph) => {
-                        if let Some(client) = &mut client {
-                            client.set_resolution(ph.width, ph.height);
-                        }
-                        glutin_ctx.resize(*ph);
-                    }
-                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                    _ => (),
-                }
-            }
-            Event::LoopDestroyed => {
-                egui_glow.destroy();
-            }
-            _ => (),
-        }
-    })
-}
-*/
+ match event {
+ WindowEvent::Resized(ph) => {
+ if let Some(client) = &mut client {
+ client.set_resolution(ph.width, ph.height);
+ }
+ glutin_ctx.resize(*ph);
+ }
+ WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+ _ => (),
+ }
+ }
+ Event::LoopDestroyed => {
+ egui_glow.destroy();
+ }
+ _ => (),
+ }
+ })
+ }
+ */
 
 #[derive(Default)]
 struct LoginScreen {
